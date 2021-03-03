02c2e7b0816501ea145df65361355830a3937e85
Incomprehens <denicenkodv@gmail.com>
2021-03-03 21:26:02 +0300

    Initial commit

---
+++ src/main/java/common/Config.java
@@ -0,0 +1,12 @@
+package common;
+
+public class Config {
+    /**
+     * Specify the browser and platform for test:
+     * CHROME_MAC
+     * CHROME_WINDOWS
+     * MOZILLA_MAC
+     * **/
+    public static final String BROWSER_AND_PLATFORM = "CHROME_WINDOWS";
+
+}
